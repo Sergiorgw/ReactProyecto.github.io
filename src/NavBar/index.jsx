192c6f92d02af6a824bed4b7f890a8fc397dc186
Sergiorgw188 <sergio150801@gmail.com>
2022-09-06 13:01:54 -0300

    cambios de estilos en navBar

--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -3,24 +3,32 @@ import CartWidget from "../cartWidget";
 
 export const NavBar = () => {
     return (
-        <div className="container">
-            <nav className="nav">
-                <div className="nav__brand">
-                    <a className="nav__link" href="empty">Mi Marca</a>
+        <div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="https://getbootstrap.com/docs/5.2/components/navbar/">Mi Marca</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="https://getbootstrap.com/docs/5.2/components/navbar/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://getbootstrap.com/docs/5.2/components/navbar/">Accesorios</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://getbootstrap.com/docs/5.2/components/navbar/">Decoración</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">
+                                <CartWidget />
+                            </a>
+                        </li>
+                    </ul>
+                    </div>
                 </div>
-                <ul className="nav__list">
-                    <li>
-                        <a className="nav__link" href="empty">Cat1</a>
-                    </li>
-                    <li>
-                        <a className="nav__link" href="empty">Cat2</a>
-                    </li>
-                    <li>
-                        <a className="nav__link" href="empty">
-                            <CartWidget />
-                        </a>
-                    </li>
-                </ul>
             </nav>
         </div>
     );
