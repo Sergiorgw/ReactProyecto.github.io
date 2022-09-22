9200b977ec97233def80849671c44f8671b8dccd
Sergiorgw188 <sergio150801@gmail.com>
2022-09-22 03:14:25 -0300

    agregando cart context

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import NavBar from "./NavBar";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemListContainer from "./ItemListContainer/index";
 import ItemDetailContainer from "./ItemDetailContainer";
-import "./App.css";
+import CartProvider from "./context/CartContext";
+import Cart from "./Cart/cart";
+
+
 
 function App() {
 	return (
 		<BrowserRouter>
-			<NavBar />
-			<Routes>
-				<Route path="/" element={<ItemListContainer />} />
-				<Route path="/categoria/:categoriaId" element={<ItemListContainer />} />
-				<Route path="/detalle/:detalleId" element={<ItemDetailContainer />} />
-			</Routes>
+			<CartProvider>
+				<NavBar />
+					<Routes>
+						<Route path="/" element={<ItemListContainer />} />
+						<Route path="/categoria/:categoriaId" element={<ItemListContainer />} />
+						<Route path="/cart" element={<Cart />} />
+						<Route path="/detalle/:detalleId" element={<ItemDetailContainer />} />
+					</Routes>
+			</CartProvider>
 		</BrowserRouter>
 	);
 }
